1a3e96122b30c0387ac46a0172343df42cc2ec44
Batmanov Vlad <v-batmanov@yandex.ru>
2022-03-23 00:09:40 +0700

    Add user route (profile, avatar)

--- controllers/users.js
+++ controllers/users.js
@@ -12,10 +12,23 @@ module.exports.getUserId = (req, res) => {
     .catch(err => res.status(500).send({ message: err.message }));
 };
 
-
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка создания пользователя' }));
+}
+
+module.exports.updateUserProfile = (req, res) => {
+  const {name, about} = req.body;
+  User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
+    .then(user => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка обновления пользователя' }));
+}
+
+module.exports.updateUserAvatar = (req, res) => {
+  const {avatar} = req.body;
+  User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true})
+    .then(user => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка аватара пользователя' }));
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const {getUsers, getUserId, createUser} = require('../controllers/users');
+const {getUsers, getUserId, createUser, updateUserProfile, updateUserAvatar} = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserId);
 router.post('/', createUser);
+router.patch('/me', updateUserProfile);
+router.patch('/me/avatar', updateUserAvatar);
 
 module.exports = router;
